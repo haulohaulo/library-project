af678f398d8fffc59d6c8100468402006398bc3f
haulo <233421461+haulohaulo@users.noreply.github.com>
2026-02-03 10:55:07 +0000

    Add Read Button

--- javascript.js
+++ javascript.js
@@ -5,6 +5,49 @@ const dialog = document.querySelector("dialog");
 const form = document.getElementById("myForm");
 const myLibrary = [];
 
+function Book(title, author, pages, readOrNot) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readOrNot = readOrNot;
+    this.id = crypto.randomUUID();
+    this.info = function() {
+      console.log(this.title + ", " + this.author + ", " + this.pages + ", " + this.readOrNot)
+    };
+}
+
+Book.prototype.read = function() {
+  this.readOrNot = "read";
+}
+
+function addBookToLibrary(title, author, pages, readOrNot) {
+  const newBook = new Book(title, author, pages, readOrNot);
+  myLibrary.push(newBook);
+  displayNewBook();
+}
+
+function displayNewBook() {
+  let bookDisplay = document.createElement("div");
+  var newBook  = myLibrary[myLibrary.length - 1];
+  bookDisplay.className = "card";
+  bookDisplay.dataset.indexNumber = newBook.id;
+  textContainer.appendChild(bookDisplay);
+  bookDisplay.textContent = newBook.title + ` ` + newBook.author + ` ` + newBook.pages + ` ` + newBook.readOrNot; 
+
+  let read = document.createElement("button");
+  read.className = "read-button";
+  read.textContent = "Read";
+  bookDisplay.appendChild(read);
+
+  let removeButton = document.createElement("button");
+  removeButton.className = "delete";
+  removeButton.textContent = "Delete";
+  bookDisplay.appendChild(removeButton);
+}
+
 
 
 
@@ -40,52 +83,23 @@ window.addEventListener("DOMContentLoaded", function(event){
           myLibrary.splice(i, 1);
         }
       }
-      
-      
-      
     })
     
   })
+
+  document.querySelectorAll(".read-button").forEach(button => {
+    button.addEventListener('click', () => {
+      const bookID = button.parentElement.dataset.indexNumber;
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].id == bookID) {
+          myLibrary[i].read();
+          
+
+        }
+      }
+    })
+  })
 });
-
-
-function Book(title, author, pages, readOrNot) {
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor");
-    }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readOrNot = readOrNot;
-    this.id = crypto.randomUUID();
-    this.info = function() {
-      console.log(this.title + ", " + this.author + ", " + this.pages + ", " + this.readOrNot)
-    };
-}
-
-function addBookToLibrary(title, author, pages, readOrNot) {
-  const newBook = new Book(title, author, pages, readOrNot);
-  myLibrary.push(newBook);
-  displayNewBook();
-}
-
-function displayNewBook() {
-  let bookDisplay = document.createElement("div");
-  var newBook  = myLibrary[myLibrary.length - 1];
-  bookDisplay.className = "card";
-  bookDisplay.dataset.indexNumber = newBook.id;
-  textContainer.appendChild(bookDisplay);
-  bookDisplay.textContent = newBook.title + ` ` + newBook.author + ` ` + newBook.pages + ` ` + newBook.readOrNot; 
-
-  let removeButton = document.createElement("button");
-  removeButton.className = "delete";
-  removeButton.textContent = "Delete";
-  bookDisplay.appendChild(removeButton);
-}
-
-
-
-
 
 
 addBookToLibrary('title1', 'author1', 'pages1', 'read')
