4f5974837a7a8bae4ca6a99b9d27820d113b4a9e
haulo <233421461+haulohaulo@users.noreply.github.com>
2026-02-05 14:57:18 +0000

    Make Read And Delete Buttons On Manually Added Books To Work

--- javascript.js
+++ javascript.js
@@ -8,7 +8,6 @@ const myLibrary = [];
 
 
 
-
 function Book(title, author, pages, readOrNot) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
@@ -35,7 +34,7 @@ function addBookToLibrary(title, author, pages, readOrNot) {
 }
 
 function displayNewBook() {
-  
+
   let bookDisplay = document.createElement("div");
   var newBook  = myLibrary[myLibrary.length - 1];
   bookDisplay.className = "card";
@@ -56,37 +55,45 @@ function displayNewBook() {
   removeButton.className = "delete";
   removeButton.textContent = "Delete";
   bookDisplay.appendChild(removeButton);
+
+  newBookAdded();
+}
+
+
+function newBookAdded(state) {
+  var newEntry = new CustomEvent('newEntry', {detail: state});
+  window.dispatchEvent(newEntry);
 }
 
 
 
-window.addEventListener("DOMContentLoaded", (event) => {
-  addButton.addEventListener("click", () => {
-    dialog.showModal();
-  })
+addButton.addEventListener("click", () => {
+  dialog.showModal();
+})
 
-  closeButton.addEventListener("click", () => {
-    dialog.close();
-  })
+closeButton.addEventListener("click", () => {
+  dialog.close();
+})
 
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const title = form.title.value;
-    const author = form.author.value;
-    const pages = form.pages.value;
-    const readOrNot = form.readOrNot.value;
-    
-    addBookToLibrary(title, author, pages, readOrNot);
+  const title = form.title.value;
+  const author = form.author.value;
+  const pages = form.pages.value;
+  const readOrNot = form.readOrNot.value;
+  
+  addBookToLibrary(title, author, pages, readOrNot);
 
-  });
+});
+
+window.addEventListener('newEntry', function (e) {
 
   document.querySelectorAll(".delete").forEach((button) => {
     button.addEventListener('click', () => {
       console.log("delete clicked");
       const bookID = button.parentElement.dataset.id;
       button.parentElement.remove();
-      
       
       for (let i = 0; i < myLibrary.length; i++) {
         if (myLibrary[i].id == bookID) {
@@ -98,21 +105,22 @@ window.addEventListener("DOMContentLoaded", (event) => {
   })
 
   document.querySelectorAll(".read-button").forEach(button => {
-    button.addEventListener('click', () => {
-      console.log("read clicked");
-      const bookContainer = button.parentElement;
-      const bookID = button.parentElement.dataset.id;
-      for (let i = 0; i < myLibrary.length; i++) {
-        if (myLibrary[i].id == bookID) {
-          myLibrary[i].read();
-          console.log(myLibrary[i]);
-          document.querySelector(`[data-id="${bookID}"] .text-container`).textContent = "read";
-        }
+  console.log("a button")
+  button.addEventListener('click', () => {
+    console.log("read clicked");
+    const bookContainer = button.parentElement;
+    const bookID = button.parentElement.dataset.id;
+    for (let i = 0; i < myLibrary.length; i++) {
+      if (myLibrary[i].id == bookID) {
+        myLibrary[i].read();
+        console.log(myLibrary[i]);
+        document.querySelector(`[data-id="${bookID}"] .text-container`).textContent = "read";
       }
-    })
+    }
   })
-
 })
+})
+  
 
 
 
